ec5086b140ce464b3be80804567dac79e76f4a65
Stearns <135378472+lilistearns@users.noreply.github.com>
2025-06-16 16:26:46 -0400

    Add password hashing + server-sided sessions

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,13 +1,34 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
+from flask_session import Session # server-side sessions
+from cachelib.file import FileSystemCache # session backend
+from flask_bcrypt import Bcrypt # password hashing
 from flask_cors import CORS
 
+# Initializations
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "secret" # set for testing purposes
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "cachelib"
+app.config['SESSION_CACHELIB'] = FileSystemCache(cache_dir='flask_session', threshold=500)
+app.config["SESSION_USE_SIGNER"] = True
+
+Session(app)
+bcrypt = Bcrypt(app)
 CORS(app)
+
 
 
 @app.route("/", methods=["GET"])
 def getHome():
-   return "Welcome..."
+    email = session.get("email")
+
+    if email is not None:
+        return jsonify({
+            "email": email,
+        })
+    else:
+        return "Not logged in."
 
 
 @app.route("/test", methods=["GET"])
@@ -29,12 +50,14 @@ def create_user():
     email = request.json.get("sendEmail")
     password = request.json.get("sendPassword")
 
+    hashed_password = bcrypt.generate_password_hash('password').decode('utf-8')
+
     if not (email and password):
         return jsonify({"message": "Please input an email and a password"}), 400
     else:
         return jsonify({
             "email": email,
-            "password": password
+            "password": hashed_password
         })
 
 
@@ -43,13 +66,23 @@ def login():
     email = request.json.get("sendEmail")
     password = request.json.get("sendPassword")
 
+    # Logic to get user account based on email and check if hashed password matches input
+    # bcrypt.check_password_hash(____, password)
+
+    session["email"] = email
+
     if not (email and password):
         return jsonify({"message": "Please enter your email and password"}), 400
     else:
         return jsonify({
             "email": email,
-            "password": password
         })
+    
+    
+@app.route("/logout", methods=["POST"])
+def logout():
+    session.pop("email", None)
+    return "Logged Out"
 
 
 
--- react-app/src/pages/home.jsx
+++ react-app/src/pages/home.jsx
@@ -3,6 +3,8 @@ export function Home() {
     return (
         <div>
             <h1>Welcome to Smart Grocery List</h1>
+            <a href = "/login"><button>Login</button></a>
+            <a href = "/signup"><button>Sign Up</button></a>
         </div>
     );
 }
